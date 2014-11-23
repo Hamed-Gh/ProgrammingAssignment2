c1dc8f1823e11f112cf4a1ecf9ec1966eaa3a941
Hamed-Gh <hmd.ghazanfari@gmail.com>
2014-11-23 20:51:57 +0330

    Assignment 2 in R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions invert a square matrix in a short time consuming using casch 
 
-## Write a short comment describing this function
+## makeCacheMatrix(x) is a function reads the 'x'.
 
 makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL       
+        set <- function(y) {    
+                x <<- y        
+                s <<- NULL      
+        }
+        get <- function() x    
+        setSolve <- function(solve) s <<- solve     
+        getSolve <- function() s        
+        list(set = set, get = get, 
+             setSolve = setSolve,         
+             getSolve = getSolve) 
+        
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve (x) function Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getSolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setSolve(s)
+        s
 }
